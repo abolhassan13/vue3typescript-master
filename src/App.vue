63c644ff199e63f6dad861c79a289ab4fddcfa1b
abolhassan13 <abolhassan13yass@gmail.com>
2023-10-23 13:07:46 +0330

    changing the format codes by Prettier and type assing

--- src/App.vue
+++ src/App.vue
@@ -3,30 +3,43 @@
     <a-space direction="vertical" :style="{ width: '100%' }" :size="[0, 48]">
       <a-layout>
         <a-layout-header :style="headerStyle" v-bind:class="tem" class="header">
-          {{ t('APP BAR') }}
+          {{ t("APP BAR") }}
         </a-layout-header>
 
         <a-layout>
-          <a-layout-sider :style="siderStyle"  class="sider" v-bind:class="side">
-            <div><router-link to="/" v-bind:class="side">{{t('Dashboard')}}</router-link> </div>
+          <a-layout-sider :style="siderStyle" class="sider" v-bind:class="side">
+            <div>
+              <router-link to="/" v-bind:class="side">{{
+                t("Dashboard")
+              }}</router-link>
+            </div>
 
-            <div><router-link to="/todos" v-bind:class="side">{{t('Todos')}}</router-link></div>
+            <div>
+              <router-link to="/todos" v-bind:class="side">{{
+                t("Todos")
+              }}</router-link>
+            </div>
 
-            <div><router-link to="/weather" v-bind:class="side">{{t('Weather')}}</router-link></div>
+            <div>
+              <router-link to="/weather" v-bind:class="side">{{
+                t("Weather")
+              }}</router-link>
+            </div>
 
-            <div><router-link to="/profile" v-bind:class="side">{{t('Profile')}}</router-link></div>
-
-
+            <div>
+              <router-link to="/profile" v-bind:class="side">{{
+                t("Profile")
+              }}</router-link>
+            </div>
           </a-layout-sider>
 
-          <a-layout-content :style="contentStyle"  class="content">
+          <a-layout-content :style="contentStyle" class="content">
             <router-view />
-          </a-layout-content>   
-
+          </a-layout-content>
         </a-layout>
 
         <a-layout-footer :style="footerStyle" v-bind:class="tem" class="footer">
-          <p>{{t('crby')}}</p>
+          <p>{{ t("crby") }}</p>
         </a-layout-footer>
       </a-layout>
     </a-space>
@@ -34,88 +47,88 @@
 </template>
 
 <script lang="ts" setup>
-import { CSSProperties, ref } from 'vue';
-import { useI18n } from "vue-i18n"
+import { CSSProperties } from "vue";
+import { useI18n } from "vue-i18n";
 
-let tem = ref('')
-tem = localStorage.getItem("thems")
+let tem: string | null;
+tem = localStorage.getItem("thems");
 
-let side = ref('')
-if(localStorage.getItem("thems") =="fallOfAutumn"){
-  side = "orangeSide"
-}else if(localStorage.getItem("thems") =="dayInSummer"){
-  side = "greenSide"
-}else if(localStorage.getItem("thems") =="SilverMoon"){
-  side = "drakSide"
+let side: string | null;
+if (localStorage.getItem("thems") == "fallOfAutumn") {
+  side = "orangeSide";
+} else if (localStorage.getItem("thems") == "dayInSummer") {
+  side = "greenSide";
+} else if (localStorage.getItem("thems") == "SilverMoon") {
+  side = "drakSide";
 }
 const { t } = useI18n({
   inheritLocale: true,
-  useScope: 'local'
-})
+  useScope: "local",
+});
 const headerStyle: CSSProperties = {
-  textAlign: 'center',
-  color: '#fff',
+  textAlign: "center",
+  color: "#fff",
   paddingInline: 50,
-  lineHeight: '40px',
-  backgroundColor: '#41a6e4',
+  lineHeight: "40px",
+  backgroundColor: "#41a6e4",
 };
 
 const contentStyle: CSSProperties = {
-  textAlign: 'center',
-  background:'#d6f0ec'
+  textAlign: "center",
+  background: "#d6f0ec",
 };
 
 const siderStyle: CSSProperties = {
-  textAlign: 'center',
-  lineHeight: '50px',
-  minWidth:'85px',
-  width: '20%',
-  backgroundColor: '#1a1d5b',
-  maxWidth: '30%'
+  textAlign: "center",
+  lineHeight: "50px",
+  minWidth: "85px",
+  width: "20%",
+  backgroundColor: "#1a1d5b",
+  maxWidth: "30%",
 };
 
 const footerStyle: CSSProperties = {
-  textAlign: 'center',
-  backgroundColor: '#41a6e4',
-  color: '#fff'
+  textAlign: "center",
+  backgroundColor: "#41a6e4",
+  color: "#fff",
 };
 </script>
 <style>
-.header{
+.header {
   height: 40px !important;
 }
-.sider{
+.sider {
   min-width: 80px !important;
-  flex: 0 0 25% !important; 
+  flex: 0 0 25% !important;
 }
-.content{
+.content {
   min-height: 550px !important;
 }
-.footer{
+.footer {
   height: 40px !important;
   padding: 0 !important;
 }
-.fallOfAutumn{
-  background: #FF5722 !important;
+.fallOfAutumn {
+  background: #ff5722 !important;
   color: #000000 !important;
 }
-.SilverMoon{
+.SilverMoon {
   background: #6c7a92 !important;
   color: #000000 !important;
 }
-.drakSide{
+.drakSide {
   background: rgb(44, 53, 68) !important;
   color: #000000 !important;
 }
-.orangeSide{
+.orangeSide {
   background: #76270f !important;
   color: #000000 !important;
 }
-.dayInSummer{
+.dayInSummer {
   background: rgb(2, 139, 0) !important;
   color: #000000 !important;
 }
-.greenSide{
+.greenSide {
   background: #114727 !important;
   color: #000000 !important;
 }
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,7 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
-import Antd from 'ant-design-vue';
-import i18n from '../src/i18n';
+import { createApp } from "vue";
+import App from "./App.vue";
+import router from "./router";
+import Antd from "ant-design-vue";
+import i18n from "../src/i18n";
 
-
-
-createApp(App).use(router).use(Antd).use(i18n).mount('#app')
+createApp(App).use(router).use(Antd).use(i18n).mount("#app");
