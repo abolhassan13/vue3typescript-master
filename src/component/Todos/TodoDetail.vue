ea31695dbd452dd5014d2f2bfa9e437efa4917b9
abolhassan13 <abolhassan13yass@gmail.com>
2023-10-25 10:35:56 +0330

    type fixing

--- src/component/Todos/TodoDetail.vue
+++ src/component/Todos/TodoDetail.vue
@@ -20,7 +20,7 @@ const emit = defineEmits<{
   (e: "delete-Task", task: task): void;
 }>();
 
-function deleteTask() {
+function deleteTask():void {
   emit("delete-Task", props.task);
 }
 </script>
